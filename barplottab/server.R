65681d8925d689d2906a28f9c670d1eb4af310f9
yshany <15618711275@163.com>
2017-02-21 01:22:58 -0500

    Add files via upload

--- barplottab/server.R
+++ barplottab/server.R
@@ -1,8 +1,9 @@
 
-
+setwd('C:/Users/sy2674/Downloads/Spr2017-proj2-grp13-master/barplottab')
 library(ggplot2)
 data1=read.csv('CleanDataFinal.csv', na.strings = "NULL")
 data1[is.na(data1)]=0
+data1[,10:18][data1[,10:18]<=200]=NA
 function(input, output) {
   
   # Fill in the spot we created for a plot
@@ -13,6 +14,13 @@ function(input, output) {
                   # Render a barplot
                   ggplot(data=data[c(input$rank:(input$rank+10)),], aes(x=data[c(input$rank:(input$rank+10)),][,'College.Name'], y=data[c(input$rank:(input$rank+10)),][,input$var],fill=data[c(input$rank:(input$rank+10)),][,input$var]))+
                     geom_bar(stat="identity")+ theme(axis.text.x=element_text(angle = -90, hjust = 0))+ylab(input$var)+scale_fill_gradient(low="white", high="blue",breaks=seq(min(data[,input$var]), max(data[,input$var]), by=0.1))+ylim(0, ylim1())+labs(fill=input$var)+xlab('College Name')
+                })
+                output$sPlot <- renderPlot({
+                  # Render a barplot
+                  InverseRank=1/data[,'Rank']
+                  Rank=data[,'Rank']
+                  ggplot(data=data, aes(x=data[,input$x], y=data[,input$y],size=InverseRank,colour=Rank))+
+                    geom_point()  + theme(axis.text.x=element_text(angle = -90, hjust = 0))+ylab(input$var)+scale_colour_gradient(low="blue", high="black",limit=(c(0,200)))+labs(fill='Rank')+xlab(input$x)+ylab(input$y)
                 })
 
 }
--- barplottab/ui.R
+++ barplottab/ui.R
@@ -9,15 +9,32 @@ fluidPage(
                     max = 100, value = 50),
         hr(),
         selectInput("var", "Varibles", 
-                    choices=names(data1)[c(9:33)])
-      ),
-  
+                    choices=names(data1)[c(9:33)]),
+        hr(),
+        hr(),
+        hr(),
+        hr(),
+        hr(),
+        hr(),
+        hr(),
+        hr(),
+        hr(),
+        hr(),
+        hr(),
+        hr(),
+        selectInput("x", "x axis", 
+                    choices=names(data1)[c(9:33)]),
+
+        selectInput("y", "y axis", 
+                  choices=names(data1)[c(9:33)])
+    ),
            
            # Copy the line below to make a slider bar 
            
 
     mainPanel(
-      plotOutput("bPlot")  
+      plotOutput("bPlot"), 
+      plotOutput("sPlot") 
     )
   
 
