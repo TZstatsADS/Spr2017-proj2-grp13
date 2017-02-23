47efc9a16de9316ea6df59019c0e77abd880fe76
myloveecho <416382216@qq.com>
2017-02-23 00:21:30 -0500

    Add files via upload

---
+++ merge/server.R
@@ -0,0 +1,138 @@
+library(leaflet)
+library(RColorBrewer)
+library(scales)
+library(lattice)
+library(dplyr)
+
+
+collegedata <- read.csv("clean3.csv")
+colnames(collegedata)[4:5] <- c("latitude","longitude")
+collegedata$COSTT4_A <- as.numeric(paste(collegedata$COSTT4_A))
+collegedata$Rank <- as.numeric(paste(collegedata$Rank))
+
+
+
+function(input, output, session) {
+  
+  observe({
+    usedcolor <- "red"
+    
+    tuition <- as.numeric(input$tuition)
+    Rank2 <- as.numeric(input$Rank)
+    SAT <- as.numeric(input$SAT) 
+    adm <- as.numeric(input$adm)
+    data<- filter(collegedata,COSTT4_A<tuition,Rank<Rank2,SAT_AVG<SAT,ADM_RATE<adm)
+    radius1 <-data$Arrest*100
+    
+    
+    if(input$color=="Rank"){
+      usedcolor <- "blue"
+      radius1 <- data$COSTT4_A
+    }else{
+      if(input$color=="population"){
+        usedcolor<- ifelse(input$sex=="men","blue","red") 
+        radius1 <- ifelse(input$sex=="men",data$UGDS_MEN*100000,data$UGDS_WOMEN*100000)
+      }
+    }
+    
+    
+    output$map <- renderLeaflet({
+      
+      leaflet(data=data) %>%
+        addTiles(
+          urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+          attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+        ) %>%setView(lng = -93.85, lat = 37.45, zoom = 4) %>%
+        clearShapes() %>%
+        addCircles(~longitude, ~latitude, radius=radius1, layerId=~UNITID,
+                   stroke=FALSE, fillOpacity=0.4, fillColor=usedcolor)
+    })
+    
+    
+    showZipcodePopup <- function(x, lat, lng) {
+      data <- data[data$UNITID == x,]
+      content <- as.character(tagList(
+        tags$h4("University:",data$INSTNM),
+        tags$strong(HTML(sprintf("information"
+        ))), tags$br(),
+        sprintf("Rank:%s",data$Rank), tags$br(),
+        sprintf("Tuition :%s  (four years)",data$CO), tags$br(),
+        sprintf("Url: %s ",data$INSTURL)
+      ))
+      leafletProxy("map") %>% addPopups(lng, lat, content, layerId =x)
+    }
+    
+    observe({
+      leafletProxy("map") %>% clearPopups()
+      event <- input$map_shape_click
+      if (is.null(event))
+        return()
+      
+      isolate({
+        showZipcodePopup(event$id, event$lat, event$lng)
+      })
+    })
+    
+    
+  })
+  
+  ## Data Explorer ###########################################
+  output$plot <- renderPlot({
+    library(fmsb)
+    name=input$school
+    
+    transpose<-read.csv("transpose.csv")
+    data=data.frame(as.numeric(as.character(transpose[7,name]))*100, (300-as.numeric(as.character(transpose[32,name])))/3, as.numeric(as.character(transpose[31,name]))/500,
+                    as.numeric(as.character(transpose[26,name]))/36*100, as.numeric(as.character(transpose[27,name]))/36*100,
+                    as.numeric(as.character(transpose[28,name]))/36*100, as.numeric(as.character(transpose[29,name]))/16)
+    
+    colnames(data)=c("admission rate" , "rank" , "Tuition", "ACT English" , "ACT Math", "ACT Writing" ,"SAT" )
+    data=rbind(rep(100,7) , rep(0,7) , data)
+    #radarchart(data)
+    radarchart( data  , axistype=1 , 
+                #custom polygon
+                pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=4 , 
+                #custom the grid
+                cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,100,20), cglwd=0.8,
+                #custom labels
+                vlcex=0.8 
+    )
+  })
+  
+  ## Data Explorer ###########################################
+  library(maps)
+  library(mapproj)
+  counties <- read.csv("orgi.csv")
+  source("helpers.R")
+  output$mapplot <- renderPlot({
+    args <- switch(input$var,
+                   "Student_Married%" = list(counties$married, "darkgreen", "Student_Married%"),
+                   "Student_Depedent%" = list(counties$dependent, "black", "Student_Depedent%"),
+                   "Student_Veteran%" = list(counties$veteran, "darkorange", "Student_Veteran%"),
+                   "Student_First_Generation%" = list(counties$first.generation, "darkviolet", "Student_First_Generation%"))
+    
+    args$min <- input$range[1]
+    args$max <- input$range[2]
+    
+    do.call(percent_map, args)
+  })
+  ####################
+  output$raderplot <- renderPlot({
+    library(fmsb)
+    #   input$satmath,input$satessay,input$satreading,input$actscore,input$moneywillingness,input$gpascore,input$studentrank
+    data=data.frame(input$satmath/8,input$satessay/8*100,input$satreading/8,input$actscore/35*100,input$moneywillingness/500,input$gpascore/4*100,input$studentrank)
+    colnames(data)=c("SAT math" , "SAT essay" , "SAT reading", "ACT" , "Financials", "GPA" ,"Rank" )
+    data=rbind(rep(100,7) , rep(0,7) , data)
+    # default       radarchart(data)
+    radarchart( data  , axistype=1 , 
+                #custom polygon
+                pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=4 , 
+                #custom the grid
+                cglcol="grey", cglty=1, axislabcol="grey", cglwd=0.8,
+                #custom labels
+                vlcex=0.8 
+    )
+  })
+  
+}
+
---
+++ merge/ui.R
@@ -0,0 +1,115 @@
+library(leaflet)
+transpose<-read.csv("transpose.csv")
+
+# Choices for drop-downs
+vars <- c(
+  "Rank" = "Rank",
+  "violence" = "violence",
+  "population ratio"="population"
+)
+var <- c("men"="men","women"="women")
+
+navbarPage("Superzip", id="nav",
+
+           tabPanel("College information map",
+                    div(class="outer",
+                        
+                        tags$head(
+                          # Include our custom CSS
+                          includeCSS("styles.css"),
+                          includeScript("gomap.js")
+                        ),
+                        
+                        leafletOutput("map", width="100%", height="100%"),
+                        
+                        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                      draggable = TRUE, top = 60, left = 20, right = "auto", bottom = "auto",
+                                      width = 300, height = "auto",
+                                      
+                                      h2("College"),
+                                      
+                                      sliderInput("Rank","Rank",min = 1,max = 300,value = 50),
+                                      sliderInput("tuition","tuition",min = 6000,max=80000,value =40000),
+                                      sliderInput("SAT","SAT",min = 900,max=1600,value =1600),
+                                      sliderInput("adm","admission rate",min = 0,max=1,value =1),
+                                      selectInput("color", "color", vars),
+                                      conditionalPanel("input.color == 'population'",
+                                                       selectInput("sex", "sex", var)
+                                      )
+                                      
+                        ),
+                        
+                        tags$div(id="cite",
+                                 'Data compiled for ', tags$em('Coming Apart: The State of White America, 1960–2010'), ' by Charles Murray (Crown Forum, 2012).'
+                        )
+                    )
+           ),
+###########################
+   tabPanel("University Information Conty Summary",
+         fluidRow(
+           column(12,
+                  h5("Create univerisity summarization graphic with information from the 2015 data.gov."),
+                  selectInput("var", 
+                              label = "Choose a variable to display",
+                              choices = c("Student_Married%", "Student_Depedent%",
+                                          "Student_Veteran%", "Student_First_Generation%"),
+                              selected = "Student Enrolled"),
+                  helpText("# student displayed here is adjusted"),
+                  sliderInput("range", 
+                              label = "Range of interest:",
+                              min = 0, max = 100, value = c(0, 100))
+           ),
+         
+         hr(),
+         
+         fluidRow(column(6, plotOutput('mapplot', height = 500)))
+         )
+   ),
+
+
+#######################################
+
+tabPanel("Student Information",
+         fluidRow(
+           column(12,
+                  h5("Hi, could you tell me a little bit about yourself? 
+                     Then we would be able to provide some analysis and related information about college for you.")
+                  ),
+           column(4,sliderInput("satessay",label = h5("SAT Essay Score"),min=2,max=8,value=6)
+           ),
+           column(4,sliderInput("satreading",label = h5("SAT Reading Score"),min=200,max=800,value=500)
+           ),
+           column(4,sliderInput("satmath",label = h5("SAT Math Score"),min=200,max=800,value=500)
+           ),
+           column(4,sliderInput("actscore",label = h5("ACT Score"),min=20,max=35,value=10)
+                  
+           ),
+           column(4,sliderInput("gpascore",label = h5("GPA Score"),min=0,max=4,value=3)
+           ),
+           column(4,sliderInput("studentrank",label = h5("Rank %"),min=1,max=100,value=50)
+           ),
+           column(4,
+                  radioButtons("nearby",label = h5("Do you want to study near school?"),choices = list("Yes"=1,"No"=2))
+           ),
+           column(4,
+                  textInput("postalcode",label = h5("Postal code"),value = "Only 5 digit number...")
+           ),
+           column(4,
+                  sliderInput("moneywillingness",label = h5("Money paid by year"),min=1000,max=50000,value=30000)
+           )
+           ),
+         
+         
+         
+         hr(),
+         
+         fluidRow(
+           column(6, plotOutput('raderplot', height = 500))
+         ),
+         fluidRow(
+           p(class = 'text-center', downloadButton('x3', 'Download Filtered Data'))
+         )
+),
+  conditionalPanel("false", icon("crosshair"))
+
+)
