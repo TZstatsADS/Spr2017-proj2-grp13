246cb0a0a03cc987d1326bf0095a73a97c54396d
myloveecho <416382216@qq.com>
2017-02-24 15:00:13 -0500

    Update ui.r

--- app/ui.r
+++ app/ui.r
@@ -1,14 +1,19 @@
 library(leaflet)
 transpose<-read.csv("transpose.csv")
-data1<-read.csv("all1.csv")
+data1<-read.csv('CleanDataFinal.csv', na.strings = "NULL")
+data1[is.na(data1)]=0
+data1[,c(10:18,31)][data1[,c(10:18,31)]<=200]=NA
+data=data1[order(data1[,'Rank']),]
+data2=data
+data2[is.na(data2)]=-999
 # Choices for drop-downs
-vars <- c(
-  "Tuition" = "Rank",
+livars <- c(
+  "Tuition" = "liRank",
   "Arrest" = "violence",
   "population ratio"="population",
   "population"="ttpopulation"
 )
-var <- c("men"="men","women"="women")
+livar <- c("men"="men","women"="women")
 
 navbarPage("College", id="nav",
            tabPanel("College information map",
@@ -28,14 +33,14 @@ navbarPage("College", id="nav",
                                       
                                       h2("College"),
                                       
-                                      sliderInput("Rank","Rank",min = 1,max = 300,value = 300),
+                                      sliderInput("liRank","Rank",min = 1,max = 300,value = 300),
                                       sliderInput("tuition","tuition",min = 6000,max=80000,value =80000),
                                       sliderInput("SAT","SAT",min = 900,max=1600,value =1600),
                                       sliderInput("ACT","ACT",min = 12,max=36,value =36),
                                       sliderInput("adm","admission rate",min = 0,max=1,value =1),
-                                      selectInput("color", "color", vars),
+                                      selectInput("color", "color", livars),
                                       conditionalPanel("input.color == 'population'",
-                                                       selectInput("sex", "sex", var)
+                                                       selectInput("sex", "sex", livar)
                                       )
                                       
                         ),
@@ -44,18 +49,20 @@ navbarPage("College", id="nav",
                                  'Data compiled for ', tags$em('Coming Apart: The State of White America, 1960–2010'), ' by Charles Murray (Crown Forum, 2012).'
                         )
                     )
-           ),        
+           ),
+           
 ###############tab university information on county level summary############
    tabPanel("University Information County Summary",
          fluidRow(
            column(12,
                   h5("Create univerisity summarization graphic with information from the 2015 data.gov."),
-                  selectInput("var", 
+                  selectInput("hkvar", 
                               label = "Choose a variable to display",
                               choices = c("Student_Married%", "Student_Depedent%",
                                           "Student_Veteran%", "Student_First_Generation%"),
-                              selected = "Student Enrolled")
-                  
+                              selected = "Student Enrolled"),
+                  sliderInput("range", label = "Range of interest:",
+                              min = 0, max = 100, value = c(0, 100))
                   
            ),
          
@@ -70,98 +77,88 @@ navbarPage("College", id="nav",
 
 tabPanel("Student Information",
          fluidRow(
-           shiny::column(6, offset = 14,
-                         h5("Hi, could you tell me a little bit about yourself? 
-                     Then we would be able to provide some analytics for you.")),
-           sliderInput("satessay",label = h5("SAT Essay Score"),min=2,max=8,value=6),
-           sliderInput("satreading",label = h5("SAT Reading Score"),min=200,max=800,value=500),
-           sliderInput("satmath",label = h5("SAT Math Score"),min=200,max=800,value=500),
-           sliderInput("actscore",label = h5("ACT Score"),min=20,max=35,value=10),
-           sliderInput("gpascore",label = h5("GPA Score"),min=0,max=4,value=3),
-           sliderInput("studentrank",label = h5("Rank %"),min=1,max=100,value=50),
-           sliderInput("moneywillingness",label = h5("Money paid by year"),min=1000,max=80000,value=30000)
+           column(12,
+                  h5("Hi, could you tell me a little bit about yourself? 
+                     Then we would be able to provide some analysis and related information about college for you.")
+                  ),
+           column(4,sliderInput("satessay",label = h5("SAT Essay Score"),min=2,max=8,value=6)
+           ),
+           column(4,sliderInput("satreading",label = h5("SAT Reading Score"),min=200,max=800,value=500)
+           ),
+           column(4,sliderInput("satmath",label = h5("SAT Math Score"),min=200,max=800,value=500)
+           ),
+           column(4,sliderInput("actscore",label = h5("ACT Score"),min=20,max=35,value=10)
+                  
+           ),
+           column(4,sliderInput("gpascore",label = h5("GPA Score"),min=0,max=4,value=3)
+           ),
+           column(4,sliderInput("studentrank",label = h5("Rank %"),min=1,max=100,value=50)
+           ),
+           column(4,
+                  sliderInput("moneywillingness",label = h5("Money paid by year"),min=1000,max=80000,value=30000)
+           )
            ),
          
+         
+         
          hr(),
          
          fluidRow(
-           shiny::column(6, offset = 2,
-                         plotOutput('raderplot', height = 500) )
-           )
-),
-######################################
-
-
-
-###############tab loan###
-
-tabPanel("Student Loan",
-         
-         googleChartsInit(),
-         
-         # Use the Google webfont "Source Sans Pro"
-         tags$link(
-           href=paste0("http://fonts.googleapis.com/css?",
-                       "family=Source+Sans+Pro:300,600,300italic"),
-           rel="stylesheet", type="text/css"),
-         tags$style(type="text/css",
-                    "body {font-family: 'Source Sans Pro'}"
+           column(6, plotOutput('raderplot', height = 500))
          ),
-         
-         h5("Student loans and earnings info"),
-         
-         hr(),
-         googleBubbleChart("chart",
-                           width="90%", height = "500px",
-                           
-                           options = list(
-                             fontName = "Source Sans Pro",
-                             fontSize = 13,
-                             # Set axis labels and ranges
-                             hAxis = list(
-                               title = "Median amount of debt($)",
-                               viewWindow = list(min = 2000 ,max = max(na.omit(data1$morethan75000)) + 5000)
-                             ),
-                             vAxis = list(
-                               title = "Mean amount of earnings($)",
-                               viewWindow = list(min = min(na.omit(data1$after6years))-10000,max = 70000 )
-                             ),
-                             # The default padding is a little too spaced out
-                             chartArea = list(
-                               top = 50, left = 75,
-                               height = "75%", width = "75%"
-                             ),
-                             # Allow pan/zoom
-                             explorer = list(),
-                             # Set bubble visual props
-                             bubble = list(
-                               opacity = 0.4, stroke = "none",
-                               # Hide bubble label
-                               textStyle = list(
-                                 color = "none"
-                               )
-                             ),
-                             # Set fonts
-                             titleTextStyle = list(
-                               fontSize = 16
-                             ),
-                             tooltip = list(
-                               textStyle = list(
-                                 fontSize = 12
-                               )
-                             )
-                           )
-         ),
-         
          fluidRow(
-           shiny::column(4, offset = 4,
-                         selectInput('state', 'State',levels(data1$STATE) )
-                         
-           )
-           
+           p(class = 'text-center', downloadButton('x3', 'Download Filtered Data'))
          )
-),   
-################################
+),
+tabPanel( 'Best Value University'  ,   fluidRow(
+  
+  column(3,sliderInput("krank", label = h3("School Rank"), min = 0, 
+                       max = 100, value = 50),
+         hr(),
+         selectInput("kvar", "Varibles", 
+                     choices=names(data1)[c(9:33)]),
+         hr(),
+         hr(),
+         hr(),
+         hr(),
+         hr(),
+         hr(),
+         hr(),
+         
+         selectInput("x", "x axis", 
+                     choices=names(data1)[c(9:33)]),
+         
+         selectInput("y", "y axis", 
+                     choices=names(data1)[c(9:33)])),
+  column(7, plotOutput("sPlot", hover = "ho"),  actionButton("go", "Find the best valued University"),
+                                                            verbatimTextOutput("info"),
+                                                            textOutput("text1")))),
+
+#####################school Recomendation##################
+tabPanel('School recommendation',  fluidRow(
+  
+  column(2,sliderInput("vr", label = "Sat verbal", min = 200, 
+                       max = 800, value = 500,step=10),
+         hr(),
+         sliderInput("mt", label = "Sat math", min = 200, 
+                     max = 800, value = 500,step=10),
+         hr(),
+         sliderInput("wr", label = "Sat writing", min = 200, 
+                     max = 800, value = 500,step=10),
+         sliderInput("act", label = "ACT", min = 1, 
+                     max = 32, value = 20),
+         sliderInput("gpa", label = "GPA", min = 0.0, 
+                     max = 5.0, value = 3.0, step= 0.1),
+         hr(),
+         
+         
+         
+
+         selectInput("major", "Any thought about major?", 
+                     choices=c('Undecided','Math & Nautural Science','Social Science','Econ, Accounting and Business','Engineering'))
+         ),column(7,plotOutput('cPlot',click = "pc2"),plotOutput('DPlot',click = "pc")),column(2,verbatimTextOutput("intro"))))
+
+,
   conditionalPanel("false", icon("crosshair"))
 
 )
