795e790c08e2976940f2e59d3c8316e66a7196a1
nkx199611 <kn2403@columbia.edu>
2017-02-24 01:37:08 -0500

    Add files via upload

---
+++ app/tab_student_loan/server.R
@@ -0,0 +1,40 @@
+
+
+library(dplyr)
+data<-read.csv("all1.csv")
+data$CONTROL <- as.factor(data$CONTROL)
+
+shinyServer(function(input, output, session) {
+  
+ 
+  defaultColors <- c("#3366cc", "#dc3912")
+  
+  series <- structure(
+    lapply(defaultColors, function(color) { list(color=color) }),
+    names = levels(data$CONTROL)
+  )
+ # year<-input$year
+ # income<-input$income
+ 
+   yearData <- reactive({
+    df <- data %>%
+      filter(STATE == input$state) %>%
+      select(INSTNM,after7years,lessthan30000,CONTROL,LOANRATE) %>%
+      arrange(CONTROL)
+    
+  })
+
+  
+  output$chart <- reactive({
+    
+    list(
+      data = googleDataTable(yearData()),
+      options = list(
+        title = sprintf(
+          "Student earnings vs student loans, %s",
+          input$year),
+        series = series
+      )
+    )
+  })
+})
